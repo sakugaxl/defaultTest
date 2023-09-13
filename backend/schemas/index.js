96e3d3c30cb1e78d96d464fe067e1b93c997cb20
sakugaxl <bekiwe01@gmail.com>
2023-09-13 03:03:07 +0200

    first commit

---
+++ backend/schemas/index.js
@@ -0,0 +1,4 @@
+import services from './services'
+import whyus from './whyus'
+
+export const schemaTypes = [services, whyus]
---
+++ backend/schemas/services.js
@@ -0,0 +1,25 @@
+export default {
+    name: 'services',
+    title: 'Services',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'imageurl',
+            title: 'ImgURL',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string'
+        },
+    ],
+}
